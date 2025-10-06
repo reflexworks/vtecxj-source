2ab42f2de49ae58a0f4940490599402742f9bb4e
noriko-terada <terada@virtual-tech.net>
2025-10-06 11:47:57 +0900

    gcloud container clusters get-credentials の引数がZONEかREGIONかを環境変数で指定するよう変更

--- vtecxj-subscriber/sh/setup-config-auth.sh
+++ vtecxj-subscriber/sh/setup-config-auth.sh
@@ -4,5 +4,5 @@ source ./settings.txt
 
 #./setup-config.sh
 
-gcloud container clusters get-credentials $CLUSTER --zone $ZONE
+gcloud container clusters get-credentials $CLUSTER $CLUSTERS_OPTION_NAME $CLUSTERS_OPTION_VALUE --quiet
 
