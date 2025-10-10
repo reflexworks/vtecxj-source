64580c730708fe735af2e3ab1da39702085ce2cc
noriko-terada <terada@virtual-tech.net>
2025-10-10 17:54:08 +0900

    requestbatchjob処理にてバッチジョブサーバのURLをプロパティファイルから取得するよう修正

--- vtecxj-batch/sh/requestbatchjob/requestbatchjob.sh
+++ vtecxj-batch/sh/requestbatchjob/requestbatchjob.sh
@@ -3,12 +3,24 @@
 # 本ファイルが配置されているディレクトリに移動
 cd `dirname $0`
 
-URL="http://vtecxbatchjob-svc/batchjob/"
+# 定数読み込み
+source ./requestbatchjob_settings.txt
+
+# プロパティファイル読み込み
+# Usage: getProperty KEY
+function getProperty() {
+    grep "^$1=" "$PROPFILE" | cut -d'=' -f2
+}
+
+URL=`getProperty '_url.batchjob'`
 
 # バッチジョブ実行管理
+#echo "[request batchjob] "`curl --version`
 #echo "[request batchjob] curl -X POST "$URL
 # -o "/dev/null" をつけるとレスポンスを出力しない。
-curl -X POST -s $URL
+#curl -X POST -s $URL
+#curl -fsS -X POST -s $URL > /dev/null
+curl --fail-with-body -X POST -s $URL > /dev/null
 
 # メッセージキュー未送信チェック
 #echo "[request batchjob] check message queue start."
